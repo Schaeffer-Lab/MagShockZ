6bd119f06d7d8943ac22570a73781e435da4f64c
dschneidinger <dschneidinger@chablis.ucla.edu>
2024-09-24 12:11:31 -0700

    Further progress on the python-fortran interface

--- python-fortran_interface/py-script.py
+++ python-fortran_interface/py-script.py
@@ -2,92 +2,75 @@
 # initialization. You could, e.g., load an ML model from a file here
 # print("I am at the module scope.")
 import numpy as np
-from scipy import interpolate
-
-if __name__ == '__main__':
-    # Compute some interpolation function and save it to disk
-    # This can be done just once by one processor and before the OSIRIS run
-    # E.g., "python py-script.py"
-
-    # Make position arrays
-    x = np.linspace(0, 10, 100)
-    y = np.linspace(0, 10, 200)
-
-    # Compute complicated function on a grid
-    # Here we keep the same indexing as the Fortran-communicated arrays, for no real reason
-    val1 = 0.2*x[np.newaxis, :] + y[:, np.newaxis]
-    val2 = x[np.newaxis, :] - y[:, np.newaxis]
-
-    # Make an interpolation object for that function (here we set the value to 0 out of bounds)
-    interp1 = interpolate.RegularGridInterpolator( (y, x), val1, method='cubic', bounds_error=False, fill_value=0 )
-    interp2 = interpolate.RegularGridInterpolator( (y, x), val2, method='cubic', bounds_error=False, fill_value=0 )
-    np.save( "interp", np.array([interp1, interp2]) )
-
+import pickle
 #-----------------------------------------------------------------------------------------
 # Functions callable by OSIRIS
 #-----------------------------------------------------------------------------------------
-def add_one( STATE ):
-    a = STATE["VAR_NAME"]
-    a = a + 1.0
-    STATE["VAR_NAME"] = a
-
-
-#-----------------------------------------------------------------------------------------
 def set_fld( STATE ):
-    # Field data is in STATE["data"]
-    # The below statement would make a copy
-    # data = STATE["data"]
-
-    # Name of the field component
-    fld = STATE["fld"]
-
+    """
+    Function to set the field data in the STATE dictionary based on the field component.
+    
+    Parameters:
+    STATE (dict): Dictionary containing the state information, including field component and positional boundary data.
+    """
+    # print("calling set_fld...")
+    
     # Positional boundary data (makes a copy, but it's small)
     x_bnd = STATE["x_bnd"]
+    # print(f"x_bnd = {x_bnd}")
 
-    # Time (in case fields are dynamic)
-    t = STATE["t"][0]
-
-    # Could make decisions based on field component
-    # if fld=="e1":
-    #     # Do something here
-    # elif fld=="b2":
-    #     # Do something else
+    # Shape of the data array
+    nx = STATE["data"].shape
+    # print(f"nx = {nx}")
 
     # Create x arrays that indicate the position (remember indexing order is reversed)
-    nx = STATE["data"].shape
     x1 = np.linspace( x_bnd[0,0], x_bnd[0,1], nx[1], endpoint=True )
     x2 = np.linspace( x_bnd[1,0], x_bnd[1,1], nx[0], endpoint=True )
-    X1, X2 = np.meshgrid( x1, x2, indexing='xy' ) # Matches Fortran array indexing
+    X1, X2 = np.meshgrid( x1, x2, indexing='xy' )
 
-    # Perform some function to fill in the field values based on the coordinates
-    STATE["data"] = 2*(X1-1) - (X2-2)
+    # Determine the filename based on the field component
+    match STATE['fld']:
+        case "e1":
+            filename = "interp/Ex-interp.pkl"
+        case "e2":
+            filename = "interp/Ey-interp.pkl"
+        case "e3":
+            filename = "interp/Ez-interp.pkl"
+        case "b1":
+            filename = "interp/magx-interp.pkl"
+        case "b2":
+            filename = "interp/magy-interp.pkl"
+        case "b3":
+            filename = "interp/magz-interp.pkl"
 
-    # Could also do this with iteration
-    # for i in range(nx[1]):
-    #     for j in range(nx[0]):
-    #         STATE["data"][j,i] = 2*(x1[i]-1) - (x2[j]-2)
+    with open(filename, "rb") as f:
+        loaded_interpolator = pickle.load(f)
+
+    STATE["data"] = loaded_interpolator((X2, X1))
 
 
 #-----------------------------------------------------------------------------------------
 def set_fld_ext( STATE ):
-    # Field data is in STATE["data"]
-    # The below statement would make a copy
-    # data = STATE["data"]
-
-    # Name of the field component
-    fld = STATE["fld"]
-
+    # print("calling set_fld_ext...")
     # Positional boundary data (makes a copy, but it's small)
     x_bnd = STATE["x_bnd"]
 
     # Time (in case fields are dynamic)
-    t = STATE["t"][0]
 
     # Could make decisions based on field component
-    # if fld=="e1":
-    #     # Do something here
-    # elif fld=="b2":
-    #     # Do something else
+    # match STATE['fld']:
+    #     case "e1":
+    #         filename = "interp/Ex-interp.pkl"
+    #     case "e2":
+    #         filename = "interp/Ey-interp.pkl"
+    #     case "e3":
+    #         filename = "interp/Ez-interp.pkl"
+    #     case "b1":
+    #         filename = "interp/magx-interp.pkl"
+    #     case "b2":
+    #         filename = "interp/magy-interp.pkl"
+    #     case "b3":
+    #         filename = "interp/magz-interp.pkl"
 
     # Create x arrays that indicate the position (remember indexing order is reversed)
     nx = STATE["data"].shape
@@ -95,70 +78,135 @@ def set_fld_ext( STATE ):
     x2 = np.linspace( x_bnd[1,0], x_bnd[1,1], nx[0], endpoint=True )
     X1, X2 = np.meshgrid( x1, x2, indexing='xy' ) # Matches Fortran array indexing
 
-    # Perform some function to fill in the field values based on the coordinates
-    STATE["data"] = -0.2*(X1-2) + (X2-1) + t
+    # # Perform some function to fill in the field values based on the coordinates
+    # with open(filename, "rb") as f:
+    #     loaded_interpolator = pickle.load(f)
 
-    # Could also do this with iteration
-    # for i in range(nx[1]):
-    #     for j in range(nx[0]):
-    #         STATE["data"][j,i] = -0.2*(x1[i]-2) + (x2[j]-1) + t
+    # STATE["data"] = loaded_interpolator((X2, X1))
+
 
 
 #-----------------------------------------------------------------------------------------
-def set_uth( STATE ):
-    # Particle positions are in STATE["x"]
-    # Thermal velocities go in STATE["u"]
-    # The below statement would make a copy
-    # x = STATE["x"]
 
-    npart = STATE["x"].shape[0]
-    x_dim = STATE["x"].shape[1]
+def set_uth_e( STATE ):
+    '''
+    In each of the above cases, the `STATE` dictionary will be prepared with the following key:
+    "x" - A real array of size `(p_x_dim, npart)` containing the positions of the particles.
 
-    # Load interpolation variable saved earlier
-    interp1, interp2 = np.load("interp.npy", allow_pickle=True)
+    The desired momentum array can then be created and set based on the positions `"x"`.  This array should be passed to the `STATE` array with the following key:
+    "u" - A real array of size `(3, npart)` containing either the thermal or fluid momenta of the particles.  **This quantity should be set to the desired momentum data.**
+    '''
+    # print("calling set_uth_e...")
+    with open('interp/vthele-interp.pkl', "rb") as f:
+        loaded_interpolator = pickle.load(f)
 
     # Prepare velocity array
-    STATE["u"] = np.zeros((npart, 3))
+    STATE["u"] = np.zeros((STATE["x"].shape[0], 3))
 
     # Set uth_x1
-    # STATE["u"][:,0] = np.abs( 0.2*STATE["x"][:,0] + 1.0*STATE["x"][:,1] )
-    STATE["u"][:,0] = interp1((STATE["x"][:,1], STATE["x"][:,0]))
-    # Set uth_x2
-    # STATE["u"][:,1] = np.abs( 1.0*STATE["x"][:,0] - 1.0*STATE["x"][:,1] )
-    STATE["u"][:,1] = interp2((STATE["x"][:,1], STATE["x"][:,0]))
-    # Set uth_x3
-    STATE["u"][:,2] = 0.1
+    STATE["u"][:,0] = loaded_interpolator((STATE["x"][:,1], STATE["x"][:,0]))
 
+    # # Set uth_x2
+    STATE["u"][:,1] = loaded_interpolator((STATE["x"][:,1], STATE["x"][:,0]))
 
+    # # Set uth_x3
+    STATE["u"][:,2] = loaded_interpolator((STATE["x"][:,1], STATE["x"][:,0]))
+
+    return STATE['u']
 #-----------------------------------------------------------------------------------------
-def set_ufl( STATE ):
-    # Particle positions are in STATE["x"]
-    # Fluid velocities go in STATE["u"]
-    # The below statement would make a copy
-    # x = STATE["x"]
 
-    npart = STATE["x"].shape[0]
-    x_dim = STATE["x"].shape[1]
+def set_uth_i( STATE ):
+    # print("calling set_uth_i...")
+    with open('interp/vthion-interp.pkl', "rb") as f:
+        loaded_interpolator = pickle.load(f)
 
     # Prepare velocity array
-    STATE["u"] = np.zeros((npart, 3))
+    STATE["u"] = np.zeros((STATE["x"].shape[0], 3))
 
-    # Set ufl_x1
-    STATE["u"][:,0] = -np.abs( 0.2*STATE["x"][:,0] + 1.0*STATE["x"][:,1] )
-    # Set ufl_x2
-    STATE["u"][:,1] = -np.abs( 1.0*STATE["x"][:,0] - 1.0*STATE["x"][:,1] )
-    # Set ufl_x3
-    STATE["u"][:,2] = -0.1
+    # Set uth_x1
+    STATE["u"][:,0] = loaded_interpolator((STATE["x"][:,1], STATE["x"][:,0]))
 
+    # # Set uth_x2
+    STATE["u"][:,1] = loaded_interpolator((STATE["x"][:,1], STATE["x"][:,0]))
+
+    # # Set uth_x3
+    STATE["u"][:,2] = loaded_interpolator((STATE["x"][:,1], STATE["x"][:,0]))
+
+    return STATE['u']
+#-----------------------------------------------------------------------------------------
+def set_ufl( STATE ):
+    # print("calling set_ufl...")
+    # Prepare velocity array
+    STATE["u"] = np.zeros((STATE["x"].shape[0], 3))
+
+    # Set uth_x1
+    with open("interp/velx-interp.pkl", "rb") as f:
+        loaded_interpolator = pickle.load(f)
+    STATE["u"][:,0] = loaded_interpolator((STATE["x"][:,1], STATE["x"][:,0]))
+
+    # # Set uth_x2
+    with open("interp/vely-interp.pkl", "rb") as f:
+        loaded_interpolator = pickle.load(f)
+    STATE["u"][:,1] = loaded_interpolator((STATE["x"][:,1], STATE["x"][:,0]))
+
+    # # Set uth_x3
+    with open("interp/velz-interp.pkl", "rb") as f:
+        loaded_interpolator = pickle.load(f)
+    STATE["u"][:,2] = loaded_interpolator((STATE["x"][:,1], STATE["x"][:,0]))
 
 #-----------------------------------------------------------------------------------------
-def set_density( STATE ):
+def load_and_interpolate_density(STATE, filename):
+    """
+    Helper function to load interpolator from a file and set the density data in the STATE dictionary.
+    
+    Parameters:
+    STATE (dict): Dictionary containing the state information, including positional boundary data.
+    filename (str): Path to the file containing the interpolator.
+    """
     # Number of points, xmin, and xmax in x and y, respectively
-    STATE["nx"] = np.array([20, 15])
-    STATE["xmin"] = np.array([0.25, 0.5])
-    STATE["xmax"] = np.array([2.75, 2.5])
+    STATE["nx"] = np.array(STATE["data"].shape)
+    STATE["xmin"] = STATE['x_bnd'][:,0]
+    STATE["xmax"] = STATE['x_bnd'][:,1]
 
-    x1 = np.linspace( STATE["xmin"][0], STATE["xmax"][0], STATE["nx"][0], endpoint=True )
-    x2 = np.linspace( STATE["xmin"][1], STATE["xmax"][1], STATE["nx"][1], endpoint=True )
-    X1, X2 = np.meshgrid( x1, x2, indexing='xy' ) # Matches Fortran array indexing
-    STATE["data"] = 0.5 * np.sin(X1 * X2 + 2 * X1) + 1.0
+    x1 = np.linspace(STATE["xmin"][0], STATE["xmax"][0], STATE["nx"][0], endpoint=True)
+    x2 = np.linspace(STATE["xmin"][1], STATE["xmax"][1], STATE["nx"][1], endpoint=True)
+    X1, X2 = np.meshgrid(x1, x2, indexing='xy')  # Matches Fortran array indexing
+
+    with open(filename, "rb") as f:
+        loaded_interpolator = pickle.load(f)
+
+    # Perform some function to fill in the field values based on the coordinates
+    return loaded_interpolator((X2, X1))
+
+#-----------------------------------------------------------------------------------------
+def set_density_e( STATE ):
+    """
+    Set the electron density data in the STATE dictionary.
+    
+    Parameters:
+    STATE (dict): Dictionary containing the state information.
+    """
+    # print("calling set_density_e...")
+    STATE['data'] = load_and_interpolate_density(STATE, "interp/edens-interp.pkl")
+
+#-----------------------------------------------------------------------------------------
+def set_density_Al( STATE ):
+    """
+    Set the aluminum density data in the STATE dictionary.
+    
+    Parameters:
+    STATE (dict): Dictionary containing the state information.
+    """
+    # print("calling set_density_Al...")
+    STATE['data'] = load_and_interpolate_density(STATE, "interp/aldens-interp.pkl")
+
+#-----------------------------------------------------------------------------------------
+def set_density_Mg(STATE):
+    """
+    Set the magnesium density data in the STATE dictionary.
+    
+    Parameters:
+    STATE (dict): Dictionary containing the state information.
+    """
+    # print("calling set_density_Mg...")
+    STATE['data'] = load_and_interpolate_density(STATE, "interp/mgdens-interp.pkl")
--- run_scripts/simulation_run2d.sh
+++ run_scripts/simulation_run2d.sh
@@ -2,22 +2,53 @@
 # arg 1 is the absolute path to your input file
 # hard code in where your project is and where osiris is on your hard drive
 
-PATHTOINPUTFILE=$1
-INPUTFILENAME=$(basename $PATHTOINPUTFILE)
-OSIRISPATH=/home/${USER}/osiris/osiris-1.0.0
-PATHTOPROJECT=/home/${USER}/MagShockZ
-OUTPUTDIR=${PATHTOPROJECT}/simulations/raw_data/${INPUTFILENAME}
-NNODES=2
+set -e  # Exit immediately if a command exits with a non-zero status
+set -x  # Print commands and their arguments as they are executed
 
+PATHTOINPUTFILE="$1"
+INPUTFILENAME=$(basename "$PATHTOINPUTFILE")
+OSIRISPATH="/home/${USER}/osiris/"
+PATHTOPROJECT="/home/${USER}/MagShockZ/"
+OUTPUTDIR="${PATHTOPROJECT}/simulations/raw_data/${INPUTFILENAME}/"
+NUM_NODES=16
 
-mkdir ${OUTPUTDIR}
-cp $1 ${OSIRISPATH}/input_file.txt
-echo "copying input file ${INPUTFILE}"
+# Validate input file
+if [ ! -f "$PATHTOINPUTFILE" ]; then
+    echo "Error: Input file does not exist or is not readable."
+    exit 1
+fi
 
-cd ${OSIRISPATH}
-./config/docker/osiris mpirun -n ${NNODES} bin/osiris-2D.e input_file.txt
+# Activate the conda environment and set PYTHONPATH
+if command -v conda &> /dev/null; then
+    eval "$(conda shell.bash hook)"
+    CURRENT_ENV=$(conda info --envs | grep '*' | awk '{print $1}')
+    if [ "$CURRENT_ENV" != "osiris" ]; then
+        conda activate osiris || { echo "Failed to activate conda environment"; exit 1; }
+    fi
+else
+    echo "Conda is not installed or not found in PATH"; exit 1;
+fi
 
-mv -f HIST/ MS/ TIMINGS/ run-info ${OUTPUTDIR}
-rm input_file.txt
+if [ -d "${OUTPUTDIR}" ]; then
+    rm -rf "${OUTPUTDIR}"/* || { echo "Failed to empty existing output directory"; exit 1; }
+else
+    mkdir -p "${OUTPUTDIR}" || { echo "Failed to create output directory"; exit 1; }
+fi
 
+cp py-script.py "${OUTPUTDIR}/py-script.py" || { echo "Failed to copy python script"; exit 1; }
+cp -r interp/ "${OUTPUTDIR}/interp/" || { echo "Failed to copy interp directory"; exit 1; }
+export PYTHONPATH=.
+
+# Copy input file
+cp "$PATHTOINPUTFILE" "${OUTPUTDIR}/input_file.txt" || { echo "Failed to copy input file"; exit 1; }
+echo "Copying input file ${INPUTFILENAME}"
+
+pushd "${OUTPUTDIR}" || { echo "Failed to change directory to OUTPUTDIR"; exit 1; }
+mpirun -n ${NUM_NODES} ${OSIRISPATH}/bin/osiris-2D.e input_file.txt || { echo "Osiris simulation failed"; popd; exit 1; }
+
+rm input_file.txt || { echo "Failed to remove input file"; popd; exit 1; }
+rm py-script.py || { echo "Failed to remove python script"; popd; exit 1; }
+rm -rf interp/ || { echo "Failed to remove interp directory"; popd; exit 1; }
+
+popd
 echo "Done"
