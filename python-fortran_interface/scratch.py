e2a114bf0474635e7393138165bb43e0115547ff
dschneidinger <dschneidinger@chablis.ucla.edu>
2024-10-02 22:40:18 -0700

    sheeeit

--- python-fortran_interface/scratch.py
+++
@@ -1,8 +0,0 @@
-def alfven(B, ne, ion_mass):
-    import numpy as np
-    m_e = 9.10938356e-28
-    c = 2.99792458e10
-    alfven = B/np.sqrt(4*np.pi*ne*ion_mass*m_e)
-    print(alfven/c)
-
-alfven(150000,5e18,400)
