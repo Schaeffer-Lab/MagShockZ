44caeffbc21b34a6c35a8a22e9e8315b7b52995f
dschneidinger <dschneidinger@chablis.ucla.edu>
2024-11-26 12:09:08 -0800

    testing

--- src/load_derived_FLASH_fields_NEW.py
+++ src/load_derived_FLASH_fields_NEW.py
@@ -4,15 +4,31 @@ import numpy as np
 def derive_fields(path_to_data:str, rqm:int = 100) -> yt.data_objects.static_output.Dataset:
     """
     args: path_to_data (str): absolute path to the FLASH dataset to load
+            rqm (int): ratio of ion mass to electron mass. Default is 100.
     Returns a yt dataset with derived fields for magnesium and aluminum number densities.
     """
     ds = yt.load(path_to_data)
 
     molar_weights = {
-        "H": 1.0078,                                                                                                       "He": 4.0026,
-        "Li": 6.94, "Be": 9.0122,                          "B": 10.81, "C": 12.011, "N": 14.007, "O": 15.999, "F": 18.998, "Ne": 20.180,
-        "Na": 22.990,"Mg": 24.305,                       "Al": 26.982, "Si": 28.085, "P": 30.974, "S": 32.06, "Cl": 35.45, "Ar": 39.948,
-    }        
+        "H": 1.0078,
+        "He": 4.0026,
+        "Li": 6.94,
+        "Be": 9.0122,
+        "B": 10.81,
+        "C": 12.011,
+        "N": 14.007,
+        "O": 15.999,
+        "F": 18.998,
+        "Ne": 20.180,
+        "Na": 22.990,
+        "Mg": 24.305,
+        "Al": 26.982,
+        "Si": 28.085,
+        "P": 30.974,
+        "S": 32.06,
+        "Cl": 35.45,
+        "Ar": 39.948,
+    }
 
     ion_1 = 'Al'
     ion_2 = 'Si'
@@ -37,8 +53,8 @@ def derive_fields(path_to_data:str, rqm:int = 100) -> yt.data_objects.static_out
         print(f"Error adding field edens: {e}")
 
     def make_ion_2_number_density(field, data):
-        ion2_percentage = (data["flash","sumy"]-1/molar_weights[ion_1])/(1/molar_weights[ion_2]-1/molar_weights[ion_1])
-        ion2_number_density = ion2_percentage*data["flash","edens"] # we did this because Zbar is ye/sumy, so we can just use edens instead to get the number density in units that are useful to osiris
+        ion2_percentage = (data["flash","sumy"] - 1/molar_weights[ion_1]) / (1/molar_weights[ion_2] - 1/molar_weights[ion_1])
+        ion2_number_density = ion2_percentage*data["flash","edens"]/yt.units.m # we did this because Zbar is ye/sumy, so we can just use edens instead to get the number density in units that are useful to osiris
         return ion2_number_density
     try:
         ds.add_field(("flash", f"{str.lower(ion_2)}dens"), function=make_ion_2_number_density, units="code_mass/code_length**3",sampling_type="cell", force_override=True) # technically the units are wrong here, should be massless
@@ -47,7 +63,7 @@ def derive_fields(path_to_data:str, rqm:int = 100) -> yt.data_objects.static_out
 
     def make_ion_1_number_density(field, data):
         ion2_percentage = (data["flash","sumy"]-1/molar_weights[ion_1])/(1/molar_weights[ion_2]-1/molar_weights[ion_1])
-        ion1_number_density = (1-ion2_percentage)*data["flash","edens"]
+        ion1_number_density = (1-ion2_percentage)*data["flash","edens"]/yt.units.code_mass
         return ion1_number_density
     try:
         ds.add_field(("flash", f"{str.lower(ion_1)}dens"), function=make_ion_1_number_density, units="code_mass/code_length**3",sampling_type="cell",force_override=True)
@@ -99,6 +115,6 @@ def derive_fields(path_to_data:str, rqm:int = 100) -> yt.data_objects.static_out
     try:
         ds.add_field(("flash", f'vth{str.lower(ion_2)}'), function=make_vth_ion_2, units="code_velocity",sampling_type="cell",force_override=True)
     except Exception as e:
-        print(f"Error adding field fvth{str.lower(ion_2)}: {e}")
+        print(f"Error adding field vth{str.lower(ion_2)}: {e}")
 
     return ds
